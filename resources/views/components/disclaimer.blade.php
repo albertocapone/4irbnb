f22701a6a70f1bc72022574daa067c45eaa568f4
Alberto Capone <albertocapone@outlook.it>
2020-08-16 00:20:39 +0200

    Disclaimer

--- resources/views/components/disclaimer.blade.php
+++ resources/views/components/disclaimer.blade.php
@@ -1,10 +1,15 @@
-<div style="position: absolute;top: 50%;left: 50%;width: 200px;height: 200px;padding: 30px;/* margin: auto auto; */border: 1px solid red;background-color: lightgoldenrodyellow;color: red;transform: translate(-50%, -50%);text-align: center;border-radius: 15px;font-weight: 900;">
+<div id="disclaimer" style="position: absolute;top: 50%;left: 50%;width: 200px;height: 200px;padding: 30px;/* margin: auto auto; */border: 1px solid red;background-color: lightgoldenrodyellow;color: red;transform: translate(-50%, -50%);text-align: center;border-radius: 15px;font-weight: 900;">
     <p>
     Questo sito esiste a scopo puramente dimostrativo.
     <br>
     Non inserire dati sensibili!
     </p>
-    <button type="button" style="position: absolute; top: 5px; right: 5px; color: red; font-size: 20px;">
+    <button id="close-disclaimer" type="button" style="position: absolute; top: 5px; right: 5px; color: red; font-size: 20px; background-color: lightgoldenrodyellow; border: none;">
         <i class="fas fa-times"></i>
     </button>
+    <script>
+        $('#close-disclaimer').click(function(){
+            $('#disclaimer').hide();
+        });
+    </script>
 </div>
