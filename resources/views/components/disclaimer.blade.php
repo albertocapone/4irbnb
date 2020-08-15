80e1e69caf07982b617eb6504a694315a0bb3033
Alberto Capone <albertocapone@outlook.it>
2020-08-16 00:14:21 +0200

    Disclaimer

--- resources/views/components/disclaimer.blade.php
+++ resources/views/components/disclaimer.blade.php
@@ -1,5 +1,10 @@
-<div style="position: absolute; top: 0; left: 0; width: 200px; height: 200px; padding: 25px; margin: auto; border: 1px solid red; background-color: pink; color: red;">
+<div style="position: absolute;top: 50%;left: 50%;width: 200px;height: 200px;padding: 30px;/* margin: auto auto; */border: 1px solid red;background-color: lightgoldenrodyellow;color: red;transform: translate(-50%, -50%);text-align: center;border-radius: 15px;font-weight: 900;">
     <p>
-    Questo sito esiste a scopo puramente dimostrativo. Non inserire dati sensibili!
+    Questo sito esiste a scopo puramente dimostrativo.
+    <br>
+    Non inserire dati sensibili!
     </p>
+    <button type="button" style="position: absolute; top: 5px; right: 5px; color: red; font-size: 20px;">
+        <i class="fas fa-times"></i>
+    </button>
 </div>
